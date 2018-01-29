de68f59625f2050f49672927d357b3e63bbba76f
skckompella <skckompella@gmail.com>
2018-01-29 08:52:06 -0500

    Adding constants

---
+++ src/constants.py
@@ -0,0 +1,22 @@
+TRAIN_FILE = "../data/cuisine.train.json"
+DEV_FILE = "../data/cuisine.dev.json"
+TEST_FILE = "../data/cuisine.unlabeled.json"
+
+IDX_TO_LABEL_FILE = "../data/idx_to_labels.pkl"
+WORD_TO_IDX_FILE = "../data/word_to_idx_file.pkl"
+
+X_TRAIN_FILE = "../data/x_train.pkl"
+Y_TRAIN_FILE = "../data/y_train.pkl"
+X_DEV_FILE = "../data/x_dev.pkl"
+Y_DEV_FILE = "../data/y_dev.pkl"
+X_TRAIN_BOW_FILE = "../data/x_train_bow.pkl"
+X_DEV_BOW_FILE = "../data/x_dev_bow.pkl"
+
+NUM_EPOCHS = 20
+BATCH_SIZE = 256
+LEARNING_RATE = 0.00001
+EMBEDDING_SIZE = 256
+DROPOUT = 0.7
+KERNEL_SIZES = [2, 3, 4]
+
+ONGPU=False
