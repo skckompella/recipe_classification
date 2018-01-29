9e656634ed52a9338294095a72d012ef8c270346
skckompella <skckompella@gmail.com>
2018-01-29 08:47:20 -0500

    Adding utils

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import json
+import cPickle as pkl
+
+# sys.setdefaultencoding('utf8')
+def read_data(data_file):
+    with open(data_file) as json_data:
+        data = json.load(json_data)
+        return data
+
+def write_data(data, data_file):
+    with open(data_file, "wb") as fp:
+        pkl.dump(data, fp)
+    print "Saved file " + data_file
+
+
+def get_accuracy(predictions, labels):
+    """
+    Returns the accuracy of the predictions, provided the labels
+    :param predictions: predictions from the model
+    :param labels: gold labels for the respective predictions
+    :return:
+    """
+
+    return float(sum(predictions == labels).data[0]) / labels.size()[0]
