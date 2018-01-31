a2f6c8d910971509810acb8294ba31f7cac1d340
skckompella <skckompella@gmail.com>
2018-01-31 14:55:48 -0500

    Adding function to read pickle file

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,35 @@ import json
 import cPickle as pkl
 
 def read_data(data_file):
+    """
+    Read from json file
+    :param data_file: Pile path
+    :return: data
+    """
     with open(data_file) as json_data:
         data = json.load(json_data)
-        return data
+    return data
 
 def write_data(data, data_file):
+    """
+    Write to pickle file
+    :param data: Data
+    :param data_file: File path
+    :return:
+    """
     with open(data_file, "wb") as fp:
         pkl.dump(data, fp)
     print "Saved file " + data_file
+
+def read_data_pkl(data_file):
+    """
+    Read from pickle file
+    :param data_file: file path
+    :return: data
+    """
+    with open(data_file, "rb") as fp:
+        data = pkl.load(fp)
+    return data
 
 
 def get_accuracy(predictions, labels):
